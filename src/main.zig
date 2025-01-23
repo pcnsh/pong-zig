c5ccc3f9d131be994837694670c8face71447557
erck <erickcintra+github@proton.me>
2025-01-22 22:23:08 -0300

    draw a inital window

--- src/main.zig
+++ src/main.zig
@@ -2,33 +2,19 @@
 
 const rl = @import("raylib");
 
+const SCREEN_WIDTH = 800;
+const SCREEN_HEIGHT = 400;
+
 pub fn main() anyerror!void {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const screenWidth = 800;
-    const screenHeight = 450;
+    rl.initWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "PONG ZIG");
+    defer rl.closeWindow();
 
-    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
-    defer rl.closeWindow(); // Close window and OpenGL context
-
-    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
+    //main game loop
+    rl.setTargetFPS(60);
+    while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
 
-        rl.clearBackground(rl.Color.white);
-
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
-        //----------------------------------------------------------------------------------
+        rl.drawRectangle(0, 0, 32, 32, rl.Color.red);
     }
 }
